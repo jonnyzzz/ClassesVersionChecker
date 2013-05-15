c26d348598c9c2ef15bb0be26117cc4d5fb4fbd8
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-05-15 13:18:05 +0200

    cleanup code

--- src/jetbrains/buildServer/tools/step/DirectoryScanStep.java
+++ src/jetbrains/buildServer/tools/step/DirectoryScanStep.java
@@ -22,20 +22,15 @@ import jetbrains.buildServer.tools.ScanStep;
 import jetbrains.buildServer.tools.fs.FSScanFile;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Collection;
-
 /**
  * @author Eugene Petrenko (eugene.petrenko@gmail.com)
  *         Date: 04.03.11 16:46
  */
 public class DirectoryScanStep implements ScanStep {
-  public void process(@NotNull ScanFile file, @NotNull Continuation c) {
+  public void process(@NotNull ScanFile file, @NotNull final Continuation c) {
     if (file instanceof FSScanFile) {
-      final Collection<ScanFile> scanFiles = ((FSScanFile) file).listFiles();
-      if (!scanFiles.isEmpty()) {
-        for (ScanFile scanFile : scanFiles) {
-          c.postTask(scanFile);
-        }
+      for (ScanFile scanFile : ((FSScanFile) file).listFiles()) {
+        c.postTask(scanFile);
       }
     }
   }
